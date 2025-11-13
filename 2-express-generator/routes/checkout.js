917613f075650b11136972666f5341626a75bfe0
amalieanderseen <aman24ab@student.cbs.dk>
2025-11-13 19:09:22 +0100

    h
    j

--- 2-express-generator/routes/checkout.js
+++ 2-express-generator/routes/checkout.js
@@ -2,17 +2,14 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-// GET /checkout → viser checkout.html
+// /checkout → viser checkout.html
 router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../public/checkout.html'));
 });
 
-// GET /checkout/gennemfoert → viser gennemfoert.html
+// /checkout/gennemfoert → viser gennemfoert.html
 router.get('/gennemfoert', function (req, res) {
   res.sendFile(path.join(__dirname, '../public/gennemfoert.html'));
 });
 
 module.exports = router;
-
-
-
--- 2-express-generator/routes/index.js
+++ 2-express-generator/routes/index.js
@@ -1,9 +1,13 @@
+
 var express = require('express');
+var path = require('path');          // 👈 tilføj
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Send den statiske HTML-fil fra /public
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
+

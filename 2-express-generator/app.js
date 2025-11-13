f288c4f0bf27e5df4d10d259a0ab3953c1bc578a
Elku966 <elku24ab@student.cbs.dk>
2025-11-13 22:23:57 +0100

    nae

--- 2-express-generator/app.js
+++ 2-express-generator/app.js
@@ -9,6 +9,21 @@ var checkoutRouter = require('./routes/checkout');
 
 var app = express();
 
+// ⭐ TILFØJET – force domain (så intet sender brugeren til localhost)
+app.use((req, res, next) => {
+  const host = req.headers.host;
+
+  // Skift til dit eget domæne
+  const correctDomain = "naee.dev";
+
+  // Hvis forespørgslen kommer fra localhost, IP eller andet host:
+  if (host !== correctDomain) {
+    return res.redirect(`https://${correctDomain}${req.originalUrl}`);
+  }
+
+  next();
+});
+
 // middleware
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,24 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/checkout', checkoutRouter);
 app.use('/', indexRouter);
 
 module.exports = app;
-
-
-// Force domain (så intet redirecter til localhost)
-app.use((req, res, next) => {
-  const host = req.headers.host;
-
-  // Skift dette til dit rigtige domæne
-  const correctDomain = "naee.dev";   //<--- RET DETTE
-
-  // Hvis host ikke er dit domæne, men fx 'localhost:3000', '127.0.0.1' osv:
-  if (host !== correctDomain) {
-    return res.redirect(`https://${correctDomain}${req.originalUrl}`);
-  }
-
-  next();
-});

6924c6882637b7f5e362d5998860b02332e3c66e
Elku966 <elku24ab@student.cbs.dk>
2025-11-24 13:28:39 +0100

    db slettet

--- 2-express-generator/app.js
+++ 2-express-generator/app.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
 
 // Routers
 var indexRouter = require('./routes/index');
 var checkoutRouter = require('./routes/checkout');
-
-// DB og cron-job (Twilio)
-require('./db');
-require('./reminderJob');
 
 var app = express();
 
@@ -44,7 +39,6 @@ app.use((req, res, next) => {
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ---------- Routes ----------

f791a0810e8d7c6d1d7799f0927f4e9352397c14
amalieanderseen <aman24ab@student.cbs.dk>
2025-11-26 13:55:11 +0100

    h
    j

--- 2-express-generator/app.js
+++ 2-express-generator/app.js
@@ -20,7 +20,7 @@ app.use(session({
   secret: 'understory-secret-key',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false } 
+  cookie: { secure: process.env.NODE_ENV === "production" }
 }));
 
 // ---------- Middleware ----------
